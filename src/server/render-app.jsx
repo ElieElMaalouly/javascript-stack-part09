83ebaa7852b4c8c9494b97903745539ec8c35015
ElieMaalouly <elie.maalouly@postlight.com>
2017-11-23 11:28:39 +0200

    fix: eslint syntax

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -5,7 +5,7 @@ import ReactDOMServer from 'react-dom/server'
 import { Provider } from 'react-redux'
 import { StaticRouter } from 'react-router'
 import Helmet from 'react-helmet'
-import { SheetsRegistry, JssProvider  } from 'react-jss'
+import { SheetsRegistry, JssProvider } from 'react-jss'
 
 import initStore from './init-store'
 import App from './../shared/app'
@@ -15,15 +15,15 @@ import { isProd } from '../shared/util'
 const renderApp = (location: string, plainPartialState: ?Object, routerContext: ?Object = {}) => {
   const store = initStore(plainPartialState)
   const sheets = new SheetsRegistry()
+  // eslint-disable-next-line
   const appHtml = ReactDOMServer.renderToString(
     <Provider store={store}>
       <StaticRouter location={location} context={routerContext}>
-        <JssProvider  registry={sheets}>
+        <JssProvider registry={sheets}>
           <App />
         </JssProvider >
       </StaticRouter>
-    </Provider>,
-  )
+    </Provider>)
   const head = Helmet.rewind()
 
   return (
--- src/shared/component/button.jsx
+++ src/shared/component/button.jsx
@@ -8,7 +8,7 @@ type Props = {
 }
 
 const Button = ({ label, handleClick }: Props) => (
-  <button onClick={handleClick} className="btn btn-primary" type="button" role="button">{label}</button>
+  <button onClick={handleClick} className="btn btn-primary" type="button">{label}</button>
 )
 
 export default Button
